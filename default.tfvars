d1586d0efb370b81b3e968ba9999cb0c32a4792e
acarir <acarir@outlook.com>
2023-08-23 17:55:09 -0500

    tfvars file added

---
+++ default.tfvars
@@ -0,0 +1,9 @@
+ec2_name = "bastion_host"
+ec2_type = "t2.micro"
+vpc_cidr = "10.0.0.0/16"
+ssh_cidr_blocks = ["10.0.0.0/16"]
+vpc_id = "terraform-test"
+subnet_id = "terraform-subnet"
+key_name = "terraform-test-key"
+public_subnet_cidr_block = "10.0.1.0/24"
+private_subnet_cidr_blocks = ["10.0.2.0/24", "10.0.3.0/24"]
--- variables.tf
+++ variables.tf
@@ -1,44 +1,35 @@
 variable "ec2_name" {
   type    = string
-  default = "bastion_host"
 }
 
 variable "ec2_type" {
   type    = string
-  default = "t2.micro"
 }
 
 variable "vpc_cidr" {
   type    = string
-  default = "10.0.0.0/16"
 }
 
 variable "ssh_cidr_blocks" {
   type    = list(string)
-  default = ["10.0.0.0/16"]
 }
 
 variable "vpc_id" {
   type    = string
-  default = "terraform-test"
 }
 
 variable "subnet_id" {
   type    = string
-  default = "terraform-subnet"
 }
 
 variable "key_name" {
   type    = string
-  default = "terraform-test-key"
 }
 
 variable "public_subnet_cidr_block" {
   type    = string
-  default = "10.0.1.0/24"
 }
 
 variable "private_subnet_cidr_blocks" {
   type    = list(string)
-  default = ["10.0.2.0/24", "10.0.3.0/24"]
 }
